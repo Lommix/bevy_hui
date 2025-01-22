2bc90a0b62a41defdc595dacf6c35bb2d0117ca6
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2025-01-22 22:11:51 +0000

    Revert "add interaction if specified"
    This reverts commit 78b0ab1f75bfeb0bf266e13dc83ff958f7a3da67.

--- crates/bevy_hui/src/build.rs
+++ crates/bevy_hui/src/build.rs
@@ -469,10 +469,6 @@ impl<'w, 's> TemplateBuilder<'w, 's> {
             self.cmd.entity(entity).insert(outline.clone());
         }
 
-        if !styles.hover.is_empty() || !styles.pressed.is_empty() {
-            self.cmd.entity(entity).insert(Interaction::default());
-        }
-
         match &node.node_type {
             // --------------------------------
             // div node
