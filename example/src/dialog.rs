8a4a2aa88b2dc2076e72406987f6aaee4e9d074f
shishanyue <62888460+shishanyue@users.noreply.github.com>
2025-07-09 22:31:40 +0800

    text_layout

---
+++ example/src/dialog.rs
@@ -0,0 +1,38 @@
+use bevy::{
+    image::ImageSamplerDescriptor,
+    prelude::*,
+    remote::{http::RemoteHttpPlugin, RemotePlugin},
+};
+use bevy_hui::prelude::*;
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(ImagePlugin {
+                default_sampler: ImageSamplerDescriptor::nearest(),
+            }),
+            RemotePlugin::default(),
+            RemoteHttpPlugin::default(),
+            HuiPlugin,
+        ))
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut cmd: Commands,
+    server: Res<AssetServer>,
+
+    mut html_funcs: HtmlFunctions,
+    mut html_comps: HtmlComponents,
+) {
+    cmd.spawn(Camera2d);
+    cmd.spawn(HtmlNode(server.load("demo/dialog.html")));
+}
+fn press(In(entity): In<Entity>) {
+    info!("press")
+}
